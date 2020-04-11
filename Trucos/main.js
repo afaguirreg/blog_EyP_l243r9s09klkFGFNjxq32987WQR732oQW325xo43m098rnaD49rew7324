cf850f7aaa0f1789f1e05220890844de117f82e2
afaguirreg <afaguirreg@users.noreply.github.com>
2020-04-10 22:47:13 -0500

    162

---
+++ Trucos/main.js
@@ -0,0 +1,32 @@
+;(function($, window, document, undefined) {
+  window.method = null;
+
+  $(document).ready(function() {
+    var input = $('#input');
+    var output = $('#output');
+    var checkbox = $('#auto-update');
+    var option = $('[data-option]');
+
+    var execute = function() {
+      try {
+        output.val(method(input.val(), option.val()));
+      } catch(e) {
+        output.val(e);
+      }
+    }
+
+    function autoUpdate() {
+      execute();
+    }
+
+    if(1) {
+      input.bind('input propertychange', autoUpdate);
+      option.bind('input propertychange', autoUpdate);
+    }
+
+    $('#execute').click(execute);
+
+    var parts = location.pathname.split('/');
+    $('a[href="' + parts[parts.length - 1] + '"]').addClass('active');
+  });
+})(jQuery, window, document);
