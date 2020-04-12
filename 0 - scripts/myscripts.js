bf4ec11c1c921c0c9571e7033ee7eaa0b661b5e8
afaguirreg <afaguirreg@users.noreply.github.com>
2020-04-12 01:13:13 -0500

    181

--- 0 - scripts/myscripts.js
+++ 0 - scripts/myscripts.js
@@ -49,9 +49,13 @@ function getDocHeight(doc) {
 function setIframeHeight(id) {
 	var ifrm = document.getElementById(id);
 	var doc = ifrm.contentDocument? ifrm.contentDocument:ifrm.contentWindow.document;
-	ifrm.style.visibility = 'hidden';
-	ifrm.style.height = "10px"; // reset to minimal height ...
-	// IE opt. for bing/msn needs a bit added or scrollbar appears
-	ifrm.style.height = getDocHeight( doc ) + 4 + "px";
-	ifrm.style.visibility = 'visible';
+	if (  iframeDoc.readyState  == 'complete' ) {
+		ifrm.style.visibility = 'hidden';
+		ifrm.style.height = "10px"; // reset to minimal height ...
+		// IE opt. for bing/msn needs a bit added or scrollbar appears
+		ifrm.style.height = getDocHeight( doc ) + 4 + "px";
+		ifrm.style.visibility = 'visible';
+		return;
+	}
+	window.setTimeout(setIframeHeight, 100);
 }
