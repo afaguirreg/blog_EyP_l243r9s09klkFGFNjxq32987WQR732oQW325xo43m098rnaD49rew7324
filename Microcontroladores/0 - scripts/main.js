a0c5589a53d654b9102c44dbfcdb9151a0fa3c05
afaguirreg <afaguirreg@users.noreply.github.com>
2020-04-12 00:43:13 -0500

    179

--- Microcontroladores/0 - scripts/main.js
+++ Microcontroladores/0 - scripts/main.js
@@ -107,7 +107,7 @@
     }
 
     $('#execute').click(execute);
-	setInterval(function(){execute();},1);
+	setInterval(function(){execute();},100);
     var parts = location.pathname.split('/');
     $('a[href="' + parts[parts.length - 1] + '"]').addClass('active');
   });
