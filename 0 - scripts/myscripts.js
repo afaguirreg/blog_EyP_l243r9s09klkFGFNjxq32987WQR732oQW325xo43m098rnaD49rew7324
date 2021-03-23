feeecc425c0e49b492e9c75acbb97423516e0ed4
afaguirreg <afaguirreg@users.noreply.github.com>
2021-03-23 09:40:04 -0500

    274

--- 0 - scripts/myscripts.js
+++ 0 - scripts/myscripts.js
@@ -65,6 +65,9 @@ setInterval(function() {
 				for (var i = 0; i < x.length; i++) {
 					var ifrm = document.getElementById(x[i].id);
 					var doc = ifrm.contentDocument? ifrm.contentDocument:ifrm.contentWindow.document;
+					var suma = docwidth % 2;
+					var resta = (docwidth + 1) % 2;
+					ifrm.style.width = parseInt( docwidth + suma - resta) + "px";
 					ifrm.style.height = getDocHeight( doc ) + "px";
 				}
 			},1000);
