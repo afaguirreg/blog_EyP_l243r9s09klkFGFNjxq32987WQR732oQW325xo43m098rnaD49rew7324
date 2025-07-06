425aabb519b72d46217bd2cb62227203daf51be6
afaguirreg <afaguirreg@users.noreply.github.com>
2025-07-06 12:22:34 -0500

    1007

--- 0 - scripts/myscripts.js
+++ 0 - scripts/myscripts.js
@@ -12,7 +12,7 @@ if (window.sidebar){
 
 function obtener_valor(variable) {
 	var remplaza = /\+/gi; 
-	var url = document.getElementById('ifrm1').src.href;
+	var url = window.location.href;
 	url = unescape(url);
 	url = url.replace(remplaza, " ");
 	var variable_may = variable;
